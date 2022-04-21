5ed985185607e0299098cefc18b876d63a84fd42
owen <owen@pop-os.localdomain>
2022-04-20 17:10:42 -0700

    Clap setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![allow(unused)]
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli{
+    /// The pdf output file (replaces the input file by default)
+    #[clap(parse(from_os_str))]
+    #[clap(short, long)]
+    output: Option<std::path::PathBuf>,
+    /// The input pdf file
+    #[clap(parse(from_os_str))]
+    file: std::path::PathBuf,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Input file {}", args.file.display());
+    println!("Output file {}", args.output.unwrap().display());
 }
